e1d3600429ef37d74aaaecdbe8ecc0ffea554c7a
Daisy <chipanal@hotmail.com>
2023-10-30 17:29:46 -0500

    Custom Appbar

---
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return SafeArea(
+      bottom: false,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            children: [
+              Icon(
+                Icons.movie_outlined,
+                color: colors.primary,
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Text(
+                  'Cinemapedia',
+                  style: titleStyle,
+                ),
+              ),
+              Spacer(),
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
